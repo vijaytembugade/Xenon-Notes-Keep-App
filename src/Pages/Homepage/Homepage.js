06259adbcea12bf5c7d7ce48523ff5e903dd6237
vijaytembugade <vijaytembugade21@gmail.com>
2022-03-27 13:29:08 +0530

    changes :some changes on headings

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -35,6 +35,33 @@ const Homepage = () => {
           <img src="/assets/Homepage/taking-notes.svg" alt="" />
         </div>
       </div>
+      <div className="secondary-homepage-part">
+        <div className="flex-container ">
+          <div className="flex-container-verticle">
+            <div className="idea-cards">CAPTURE IDEAS</div>
+            <div className="idea-cards">MARKUP</div>
+          </div>
+          <img
+            className="secondary-part-image"
+            src="/assets/Homepage/processes.svg"
+            alt=""
+          />
+          <div className="flex-container-verticle">
+            <div className="idea-cards">MULTITASK</div>
+            <div className="idea-cards">ORGANIZED</div>
+          </div>
+        </div>
+        <div className="flex-container-verticle homepage-secondary-heading">
+          <div className="heading-large  dark-text">
+            FALL IN <span className="secondary-text">LOVE </span>
+            WITH k<span className="ternary-text">X</span>EEP
+          </div>
+          <span className="secondary-tagline">
+            Follow your daily routine and create new experience with k
+            <span className="ternary-text">X</span>eep.
+          </span>
+        </div>
+      </div>
     </>
   );
 };
