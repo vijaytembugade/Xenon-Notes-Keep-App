4d7157d353e893354019c98f14de54b568eb3456
vijaytembugade <vijaytembugade21@gmail.com>
2022-03-30 12:04:59 +0530

    Create new note

--- src/Components/AllNotes/AllNotes.js
+++ src/Components/AllNotes/AllNotes.js
@@ -6,31 +6,56 @@ import NoteEditor from "../NoteEditor/NoteEditor";
 
 const AllNotes = () => {
   const [value, setValue] = useState("");
+  const [showNoteEditor, setShowNoteEditor] = useState(false);
+
   return (
     <div className="allnotes-container">
-      {/* <NoteEditor /> */}
-      <div className="note-editor">
-        <div className="title title-md secondary-text">Create New Note</div>
-        <input className="note-title" type="text" placeholder="Title of Note" />
-        <div className="new-note">
-          <Quill value={value} setValue={setValue} />
+      {!showNoteEditor && (
+        <button
+          className="btn btn-ternary-outline"
+          onClick={() => setShowNoteEditor(true)}
+        >
+          <span class="material-icons">add_circle</span>
+          Create New Note
+        </button>
+      )}
+      {showNoteEditor && (
+        <div className="note-editor">
+          <div className="title title-md secondary-text">Create New Note</div>
           <input
-            className="note-label"
+            className="note-title"
             type="text"
-            placeholder="Label of Note"
+            placeholder="Title of Note"
           />
-          <div className="note-color-selector">
-            <input type="radio" className="secondary-radio-text" name="color" />
-            <input type="radio" className="ternary-radio-text" name="color" />
-            <input type="radio" className="success-radio-text" name="color" />
-            <input type="radio" className="warning-radio-text" name="color" />
-          </div>
-          <div className="flex-container">
-            <button className="btn btn-primary-outline">Save</button>
-            <button className="btn  btn-primary-outline">Cancle</button>
+          <div className="new-note">
+            <Quill value={value} setValue={setValue} />
+            <input
+              className="note-label"
+              type="text"
+              placeholder="Label of Note"
+            />
+            <div className="note-color-selector">
+              <input
+                type="radio"
+                className="secondary-radio-text"
+                name="color"
+              />
+              <input type="radio" className="ternary-radio-text" name="color" />
+              <input type="radio" className="success-radio-text" name="color" />
+              <input type="radio" className="warning-radio-text" name="color" />
+            </div>
+            <div className="flex-container">
+              <button className="btn btn-primary-outline">Save</button>
+              <button
+                className="btn  btn-primary-outline"
+                onClick={() => setShowNoteEditor(false)}
+              >
+                Cancle
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
       <div dangerouslySetInnerHTML={{ __html: value }}></div>
     </div>
